2ab7b0b5c68b489456d1a038cb4d9c11bc88219c
jinsugyeong <apr15th@naver.com>
2024-02-28 22:25:13 +0900

    Mission02. 소켓 서버, 소켓 클라이언트 기능으로 데이터 주고받기

---
+++ NodeExample1/ch04_mission02_client.js
@@ -0,0 +1,24 @@
+// 서버에 연결
+var net = require('net');
+var hostname = 'localhost';
+var port = 3000;
+
+var client = new net.Socket();
+client.setEncoding('utf8');
+client.connect(port, hostname, function() {
+	this.setEncoding('utf8'); 
+    console.log('서버에 연결됨 -> ' + hostname + ':' + port);
+	client.write('안녕');
+
+	// 서버로부터 데이터를 받았을 때 발생하는 이벤트
+	client.on('data', function(data) {
+		console.log('서버로부터 받은 데이터 -> ' + data);
+		
+		// 클라이언트 연결 종료
+		client.destroy();
+	});
+	
+	client.on('close', function() {
+		console.log('연결 끊어짐.');
+	});
+});
