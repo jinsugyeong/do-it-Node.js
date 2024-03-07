9e226b20362cb3d1bb42f0f2c160513fd0cf7dc6
jinsugyeong <apr15th@naver.com>
2024-03-07 23:32:18 +0900

    04-4. 로그 파일 남기기 json형식 오류 수정 츨처 : https://xively.tistory.com/21

--- NodeExample1/ch04_test15.js
+++ NodeExample1/ch04_test15.js
@@ -1,59 +1,65 @@
-var winston = require('winston');    				// 로그 처리 모듈
-var winstonDaily = require('winston-daily-rotate-file');    	// 로그 일별 처리 모듈
-var moment = require('moment');    				// 시간 처리 모듈
+const winston = require('winston');
+require('winston-daily-rotate-file');
+const logDir = './log';
 
-function timeStampFormat() {
-    return moment().format('YYYY-MM-DD HH:mm:ss.SSS ZZ'); // '2016-05-01 20:14:28.500 +0900'
-};
+const levels = {
+    error: 0,
+    warn: 1,
+    info: 2,
+    http: 3,
+    debug: 4,
+}
 
-var logger = new (winston.Logger)({
+const level = () => {
+    const env = process.env.NODE_ENV || 'development'
+    const isDevelopment = env === 'development'
+    return isDevelopment ? 'debug' : 'warn'
+}
+
+const colors = {
+    error: 'red',
+    warn: 'yellow',
+    info: 'green',
+    http: 'magenta',
+    debug: 'blue',
+}
+
+winston.addColors(colors);
+
+const format = winston.format.combine(
+    winston.format.timestamp({ format: ' YYYY-MM-DD HH:MM:SS ||' }),
+    winston.format.colorize({ all: true }),
+    winston.format.printf(
+        (info) => `${info.timestamp} [ ${info.level} ] ▶ ${info.message}`,
+    ),
+)
+
+const logger = winston.createLogger({
+
+    format,
+    level: level(),
     transports: [
-        new (winstonDaily)({
-            name: 'info-file',
-            datePattern: 'YYYY-MM-DD', // 파일 날짜 형식
-            dirname: './log', // 파일 경로
-            filename: `server_%DATE%.log`, // 파일 이름
-            colorize: false,
-            maxsize: 50000000,
-            maxFiles: 1000,
-            level: 'info',	//(0)debug < info < notice < warnig < error < crit < alert < emerg(7)
-            showLevel: true,
-            json: false,
-            timestamp: timeStampFormat
+        new winston.transports.DailyRotateFile({
+            level: 'info',
+            datePattern: 'YYYY-MM-DD',
+            dirname: logDir,
+            filename: `%DATE%.log`,
+            zippedArchive: true,	
+            handleExceptions: true,
+            maxFiles: 30,  
         }),
-        new (winston.transports.Console)({
-            name: 'debug-console',
-            colorize: true,
-            level: 'debug',
-            showLevel: true,
-            json: false,
-            timestamp: timeStampFormat
-        })
-    ],
-    exceptionHandlers: [
-        new (winstonDaily)({
-            name: 'exception-file',
-            datePattern: 'YYYY-MM-DD', // 파일 날짜 형식
-            dirname: './log', // 파일 경로
-            filename: `exception_%DATE%.log`, // 파일 이름
-            colorize: false,
-            maxsize: 50000000,
-            maxFiles: 1000,
+        new winston.transports.DailyRotateFile({
             level: 'error',
-            showLevel: true,
-            json: false,
-            timestamp: timeStampFormat
+            datePattern: 'YYYY-MM-DD',
+            dirname: logDir + '/error',  
+            filename: `%DATE%.error.log`,
+            zippedArchive: true,
+            maxFiles: 30,
         }),
-        new (winston.transports.Console)({
-            name: 'exception-console',
-            colorize: true,
-            level: 'debug',
-            showLevel: true,
-            json: false,
-            timestamp: timeStampFormat
+        new winston.transports.Console({
+            handleExceptions: true,
         })
     ]
 });
-
 
 module.exports = logger;
